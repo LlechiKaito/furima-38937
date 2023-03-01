3c4d12295f313fbd73d9ac89ea90731cddbba6d5
LlechiKaito <124239629+LlechiKaito@users.noreply.github.com>
2023-03-01 20:48:41 +0900

    usersテーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
+  #正規表現によるvalidatesがあります。
 end
