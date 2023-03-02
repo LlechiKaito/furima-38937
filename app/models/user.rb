c65ffb80ff00a80d5bbfa73f4382868adbfcc538
LlechiKaito <124239629+LlechiKaito@users.noreply.github.com>
2023-03-02 14:49:09 +0900

    userモデルのvalidationの正規表現

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,16 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'カタカナを使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+  
   #正規表現によるvalidatesがあります。
 end
