f1f40715539a512b854320564c7dff69b853386a
LlechiKaito <124239629+LlechiKaito@users.noreply.github.com>
2023-03-02 18:44:20 +0900

    ユーザー管理機能修正その５

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
     validates :first_name
     validates :last_name
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶ-ー]+\z/, message: 'カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'カタカナを使用してください' } do
     validates :first_name_kana
     validates :last_name_kana
   end
