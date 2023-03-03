93af86e55fe7dbbdce75bb03e453ff8dd5d5471d
LlechiKaito <124239629+LlechiKaito@users.noreply.github.com>
2023-03-03 14:34:33 +0900

    商品画像について

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   validates :name, presence: true
   validates :price, presence: true
@@ -9,5 +10,6 @@ class Item < ApplicationRecord
   validates :shipping_area_id, presence: true
   validates :days_left_id, presence: true
   validates :category_id, presence: true
+  validates :image, attached_file_presence: true
 
 end
