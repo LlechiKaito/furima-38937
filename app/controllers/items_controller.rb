96f830e9143c8cb31b19a9ed291f6789667a887f
LlechiKaito <124239629+LlechiKaito@users.noreply.github.com>
2023-03-03 14:24:13 +0900

    商品出品ページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    if user_signed_in?
+      @item = Item.new
+    else
+      render action: :index
+    end
+  end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,13 @@
+class Item < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :content, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_fee_id, presence: true
+  validates :shipping_area_id, presence: true
+  validates :days_left_id, presence: true
+  validates :category_id, presence: true
+
+end
