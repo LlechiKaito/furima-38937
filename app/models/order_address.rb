6ee4bb42bae1bdc3e42d77b01e368880069d0b9d
LlechiKaito <llechi0420@gmail.com>
2023-03-09 15:30:57 +0900

    商品購入機能のmodelとデータベースの実装

---
+++ app/models/order_address.rb
@@ -0,0 +1,20 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_code, :shipping_area_id, :municipality, :address, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_code
+    validates :municipality
+    validates :address
+    validates :building_name
+    validates :phone_number
+  end
+  validates :shipping_area_id, numericality: { other_than: 0, message: "can't be blank" }
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, shipping_area_id: shipping_area_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
+end
